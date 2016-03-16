c0d7e19e6e057d58e30b081fb234bb08fa26f5db
z1mmz <wolfz@live.com>
2016-03-16 14:48:38 +1100

    Check in
    Created basic states

--- src/com/sept01/main/State.java
+++ src/com/sept01/main/State.java
@@ -1,5 +1,11 @@
 package com.sept01.main;
 
 public class State {
+String name;
+	public State(String name){
+		this.name = name;
+	}
+	
+	
 
 }
--- src/com/sept01/main/Weather.java
+++ src/com/sept01/main/Weather.java
@@ -1,5 +1,33 @@
 package com.sept01.main;
-
+import org.json.JSONArray;
+import com.jaunt.*;
 public class Weather {
-
+//abbreviations for BOM webscraping
+String[] statesAbv = {"vic", "nsw", "tas", "wa", "sa", "nt", "qld", "ant"};
+//Array of state objects
+State[] states;
+ public Weather(){
+	 initialize(states);
+ }
+ 
+ private boolean initialize(State[] state){
+	 //initialize states with abbreviations as name
+	 
+	for(int i = 0; i < states.length; i++){
+		states[i] = new State(statesAbv[i]);
+	} 
+	 return false;
+	 
+	 
+ }
+ 
+ protected State getStateWeather(String state){
+	
+	 
+	 return null;
+	  
+ }
+ 
+ 
+ 
 }
