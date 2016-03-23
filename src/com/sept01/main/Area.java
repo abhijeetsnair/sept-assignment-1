2043932fbe4b12077689c642911c6ddb42bf5604
z1mmz <wolfz@live.com>
2016-03-23 12:57:52 +1100

    Updates area class
    Area class now gets links for all weather stations in its area

--- src/com/sept01/main/Area.java
+++ src/com/sept01/main/Area.java
@@ -1,15 +1,29 @@
 package com.sept01.main;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.jaunt.Element;
+import com.jaunt.Elements;
 
 public class Area {
 private String name;
 private String id;
+Element weatherStationTable;
 ArrayList<WeatherStation> weatherStations = new ArrayList<>();
-   public Area(String name, String id){
+   public Area(String name, String id,Element table){
       this.name = name;
       this.id = id;
+      //strip the table and get just the links
+      weatherStationTable =  table.findEvery("<a href>");;
       
+      //loop just to test that we are getting just the stations for this area remove it to tidy up
+ 	 Iterator<Element> titr = ((Elements) weatherStationTable).iterator();
+ 	 while(titr.hasNext()){	
+ 		System.out.println(name);
+		 System.out.println("weather station: "+titr.next().getAtString("href"));
+		 
+	 }
    }
    public String getName(){
       return name;
--- src/com/sept01/main/State.java
+++ src/com/sept01/main/State.java
@@ -7,7 +7,6 @@ import com.jaunt.JauntException;
 import com.jaunt.UserAgent;
 public class State {
 String name;
-
 //Currently only holding strings to weather station pages
 ArrayList<Object> areaNames = new ArrayList<>();
 ArrayList<Area> areas = new ArrayList<>();
@@ -23,20 +22,30 @@ ArrayList<Area> areas = new ArrayList<>();
 			 userAgent.visit("http://www.bom.gov.au/"+name+"/observations/"+name+"all.shtml");                        //visit a url  
 //			 Elements elements = userAgent.doc.findEvery("<a href>");
 			 Elements elements = userAgent.doc.findEvery("<h2>");
+			 Elements tElements = userAgent.doc.findEvery("<table>");
 			 Iterator<Element> itr = elements.iterator();
+			 Iterator<Element> titr = tElements.iterator();
 			 int x = 0;
 			 while(itr.hasNext()){
 //				 if(elements.getElement(x).getAtString("href").contains("product")){
 			    if(elements.getElement(x).innerText().toUpperCase().compareTo("WEATHER STATION INFORMATION") != 0){
 					 //System.out.println("weather station: "+elements.getElement(x).getAtString("href")); 
 //					 weatherStations.add(elements.getElement(x).getAtString("href"));
-			       areas.add(new Area(elements.getElement(x).innerText(), "t"+elements.getElement(x).getAtString("id")));
+			    	
+			       areas.add(new Area(elements.getElement(x).innerText(), "t"+ elements.getElement(x).getAtString("id"),userAgent.doc.findEvery("<table id="+"t"+ elements.getElement(x).getAtString("id")+">")));
 			       areaNames.add(elements.getElement(x).innerText());
 				 }
 				 itr.next();
 				 x++;
 			 }
-			 
+//			 while(titr.hasNext()){
+//				 Elements aElements = tElements.findEvery("<a href>");
+//				 System.out.println(titr.next().getAtString("id"));
+//				 for(Element element : aElements){
+//				 //System.out.println("weather station: "+element.getAtString("href"));
+//			 }
+//				 
+//			 }
 		}catch(JauntException e){
 			 System.err.println(e);
 		}
