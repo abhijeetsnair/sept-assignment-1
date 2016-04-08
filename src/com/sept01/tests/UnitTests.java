a8ca2fc66b886c7a3ef89d191a947f83c550caa8
Sarah Giapitzakis <s3477089@student.rmit.edu.au>
2016-04-08 18:58:28 +1000

    Setting up initial junit testing framework
    Setting up initial junit testing framework

---
+++ src/com/sept01/tests/UnitTests.java
@@ -0,0 +1,16 @@
+package com.sept01.tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UnitTests{
+	
+	@Test
+	public void testTest(){
+		int var1 = 0;
+		int var2 = 0;
+		
+	    // assert statements
+	    assertEquals(var1, var2);
+	}
+}
