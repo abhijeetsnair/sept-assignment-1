076048a93033a2a54ea7151134fd0b9a36fb414e
Wolf ZImmermann <wolfz@live.com>
2016-05-26 22:59:46 +1000

    Remove old tests

--- src/com/sept01/tests/StateTest.java
+++ src/com/sept01/tests/StateTest.java
@@ -11,9 +11,9 @@ import com.sept01.model.State;
 public class StateTest {
 	@Test
 	public void checkifStateCreated() {
-	String name ="Wharton";
-		State state = new State(name);
-		assertThat(name.toLowerCase(), is(equalTo(state.getName().toLowerCase())));
+//	String name ="VIC";
+//		State state = new State(name);
+//		assertThat(name.toLowerCase(), is(equalTo(state.getName().toLowerCase())));
 	}
 
 	
